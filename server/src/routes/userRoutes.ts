8672fce8943f64695a541192e5c9776b6ace2ebd
shaked4041 <shaked4041@gmail.com>
2025-01-29 12:41:41 +0200

    Fix missing type definitions

--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response, Router } from 'express';
+import { Request, Response, Router } from 'express';
 import { identifyCurrentUser } from '../utils/funcs';
 
 const router = Router();
